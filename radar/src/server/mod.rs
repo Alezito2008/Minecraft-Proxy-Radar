7dc70a9e2152a6811738ad3131dcc24acd9d8360
Alezito2008 <alejandro.kretzig@gmail.com>
2026-04-19 03:35:14 -0300

    Serve frontend from rust server

--- radar/src/server/mod.rs
+++ radar/src/server/mod.rs
@@ -1,6 +1,9 @@
+use std::path::PathBuf;
+
 use crate::{server::handlers::ws_handler};
 use axum::{Router, routing};
 use tokio::sync::broadcast;
+use tower_http::services::ServeDir;
 
 mod handlers;
 
@@ -10,8 +13,14 @@ struct AppState {
 }
 
 pub fn create_router(tx: broadcast::Sender<String>) -> Router {
+    let web_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
+        .join("web");
+
     let state = AppState { tx };
     Router::new()
         .route("/ws", routing::get(ws_handler))
+        .fallback_service(ServeDir::new(web_dir))
         .with_state(state)
 }
